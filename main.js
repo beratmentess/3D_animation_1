3721768d0f5ac374939dc1379284603e82db86e1
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2024-07-20 02:33:04 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const randomCharacter = () => chars[Math.floor(Math.random() * chars.length)];
+const randomString = (length) =>
+  Array.from(Array(length)).map(randomCharacter).join("");
+
+const cardHover = (card, letters, e) => {
+  const rect = card[0].getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+
+  letters[0].style.setProperty("--x", `${x}px`);
+  letters[0].style.setProperty("--y", `${y}px`);
+
+  letters[0].innerText = randomString(2000);
+};
+
+const card = $(".card-hover-container");
+card.each(function () {
+  $(this).on("mousemove", (e) =>
+    cardHover($(this), $(this).children(".card-bg-characters"), e)
+  );
+  $(this).on("touchmove", (e) =>
+    cardHover($(this), $(this).children(".card-bg-characters"), e)
+  );
+});
